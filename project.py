03182385018d722bb7ea7034f3016ffbd78b8f2d
AnonimusZ6 <59754248+AnonimusZ6@users.noreply.github.com>
2022-05-10 22:15:32 +0800

    Update project.py

--- project.py
+++ project.py
@@ -6,15 +6,20 @@ web_app = Flask(__name__)
 #First page
 @web_app.route('/')
 def entry() -> 'html':
-
     return render_template('entry.html', titles='Welcome to the randnumber 2.0')
 
 
 
+def logg(reqq, results) -> xz:
+    """log"""
+    with open('logi.log', 'a') as log:
+        print(reqq, results, file=log)
+  
 @web_app.route('/res', methods=['POST'])
 def res() -> 'html':
     """Results"""
     numberss = request.form['numbers']
+    logg(request, resultss)
     return render_template('results.html',
     numbb = numberss,
     resultss = randd(numberss))
